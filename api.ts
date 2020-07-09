0887c2ad5fa20d9e61cc67510afd38b93e77bb45
e2517 <carlos@achoweb.es>
2020-07-09 14:58:19 +0100

    Deno Server

--- api.ts
+++ api.ts
@@ -1,13 +1,13 @@
-import { Router } from "./deps.ts";
+import { Router, Request, Response } from "./deps.ts";
 
 const api = new Router();
 
-api.get('/', (req, res) => {
+api.get('/', (Request: req, Response: res) => {
 
     res.status(200).send({ status: 'Deno running' });
 })
 
-api.get('/:id', (req, res) => {
+api.get('/:id', (Request: req, Response: res) => {
 
     res.status(200).send({ param: req.params.id });
 });
